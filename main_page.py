f13bca4fa1510a00710ee7beef0c45bd99af6788
poha_god <76470661+shubhamsd4@users.noreply.github.com>
2023-02-22 13:40:11 +0530

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -310,8 +310,8 @@ with st.container():
                 postpaid_base_price_obj = AasaanPostPaidBasePriceCalculation(max_txn, base_price, base_price_comp, no_of_slabs)
                 postpaid_baseprice_pricing = postpaid_base_price_obj.aasaan_postpaid_base_price()
                 postpaid_baseprice_comp_pricing = postpaid_base_price_obj.aasaan_postpaid_base_price_comp()
-                #monthly_txn_bprice = max(max_txn)
-                est_revenue_bprice = est_yearly_revenue(100, float(aov_bprice))
+                monthly_txn_bprice = max(max_txn)
+                est_revenue_bprice = est_yearly_revenue(monthly_txn_bprice, float(aov_bprice))
                 incr_revenue_perc_bprice = incr_rev_perc(est_revenue_bprice)
                 incr_revenue_bprice = incr_rev(est_revenue_bprice, incr_revenue_perc_bprice)    
                 postpaid_base_price_button = st.button("Calculate Price", key="postpaid base price")
@@ -320,8 +320,6 @@ with st.container():
                     st.metric("Aasaan Incremental Revenue", incr_revenue_bprice)
                 with comp_column:
                     st.metric("Competitor", postpaid_baseprice_comp_pricing)
-
-                    
                     #st.write(monthly_txn_bprice)
                     #Competitor Function Values   
                     if aov_bprice and monthly_txn_bprice:
