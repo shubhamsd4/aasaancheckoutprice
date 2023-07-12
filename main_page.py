1efedee585827b6440eb1e9af8b6af760457bc61
shubhamsd4 <76470661+shubhamsd4@users.noreply.github.com>
2023-07-12 14:53:46 +0530

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -477,7 +477,7 @@ if country_dropdown == 'UAE':
                 if prepaid_button:
                     st.metric("Aasaan Yearly Cost",convert_to_uae_currency(UAEaasaan_prepaid(selected_subscription_plan)))
                     st.metric("Yearly Incremental Revenue", convert_to_uae_currency(incr_revenue_prepaid))
-                    st.metric("Months to breakeven", round(breakeven(aasaan_prepaid(selected_subscription_plan), incr_revenue_prepaid),2))
+                    st.metric("Months to breakeven", round(breakeven(UAEaasaan_prepaid(selected_subscription_plan), incr_revenue_prepaid),2))
                    
         else:
             st.empty()
